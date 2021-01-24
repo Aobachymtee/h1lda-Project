8c048c06fe681e6ed0954ab91f751999ec3c9fbc
Aobachymtee <62795306+Aobachymtee@users.noreply.github.com>
2021-01-24 23:26:34 +0700

    Update analytics.js

--- assets/javascript/analytics.js
+++ assets/javascript/analytics.js
@@ -3,5 +3,5 @@
 s=b.getElementsByTagName(x)[0];u.src=i;s.parentNode.insertBefore(u,s)}
 (window,document,'ga','script','https://www.google-analytics.com/analytics.js');
 
-ga('create', 'UA-122247573-1', 'obnoxious.club');
+ga('create', 'UA-122247573-1', 'h1lda.ml');
 ga('send', 'pageview');
